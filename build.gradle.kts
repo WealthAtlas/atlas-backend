2133b9461ac9530265ecb190d7876bed7ed25a29
Prasanna Anbazhagan <prasannaa@Prasannas-MBP.communityfibre.co.uk>
2025-04-12 13:01:23 +0100

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
-    kotlin("jvm") version "2.1.20" apply false
-    kotlin("plugin.spring") version "2.1.20" apply false
-    id("org.springframework.boot") version "3.2.0" apply false
-    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") apply false
+    kotlin("plugin.spring") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management") apply false
 }
 
 allprojects {
